6b0cc4b60eeeb7543c1606ca76b08c54bd52270f
Anik Mohammad <anik7303@gmail.com>
2022-12-10 00:01:42 +0600

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createGlobalStyle } from "styled-components";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import App from "./App";
+
+const GlobalStyles = createGlobalStyle`
+  :root {
+    --white: #f7f7f7;
+    --black: #222222;
+    --gray: #cccccc;
+    --red: #ca0f0f;
+    --primary: #0871b8;
+    --secondary: #07a563;
+  }
+
+  *, *::before, *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: sans-serif;
+  }
+
+  html {
+    font-size: 100%;
+  }
+
+  body {
+    width: 100%;
+    min-height: 100vh;
+    font-size: 1rem;
+    font-weight: 400;
+    background-color: var(--white);
+  }
+
+  #root {
+    width: 100%;
+  }
+`;
+
+const domNode = document.getElementById("root") as HTMLDivElement;
+const root = ReactDOM.createRoot(domNode);
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
